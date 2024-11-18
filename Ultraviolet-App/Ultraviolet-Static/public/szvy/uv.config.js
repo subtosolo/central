11d659d3a23569d92aebaaafa7f1f7dba874f449
szvy <158786105+szvy@users.noreply.github.com>
2024-11-18 16:25:38 -0600

    fix uvconfig

--- Ultraviolet-App/Ultraviolet-Static/public/szvy/uv.config.js
+++ Ultraviolet-App/Ultraviolet-Static/public/szvy/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/szvy/",
+  prefix: "/szvy/szvy/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.base64.encode,
   decodeUrl: Ultraviolet.codec.base64.decode,
